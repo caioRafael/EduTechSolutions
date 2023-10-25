454470840771a996af1d10a6894e507c34adbd51
Paulo <silvavitorlopes711@gmail.com>
2023-10-24 23:57:36 -0300

    fix: remove unused things

--- components/features.tsx
+++ components/features.tsx
@@ -1,97 +1,163 @@
-'use client'
+"use client";
 
-import { useState, useRef, useEffect } from 'react'
-import { Transition } from '@headlessui/react'
-import Image from 'next/image'
-import FeaturesBg from '@/public/images/features-bg.png'
-import FeaturesElement from '@/public/images/features-element.png'
-import Cursos from '@/public/images/cursos.png'
-import Avisos from '@/public/images/avisos.png'
-import Aula from '@/public/images/aula.png'
+import { useState, useRef, useEffect } from "react";
+import { Transition } from "@headlessui/react";
+import Image from "next/image";
+import FeaturesBg from "@/public/images/features-bg.png";
+import FeaturesElement from "@/public/images/features-element.png";
+import Cursos from "@/public/images/cursos.png";
+import Avisos from "@/public/images/avisos.png";
+import Aula from "@/public/images/aula.png";
 
 export default function Features() {
-  
-  const [tab, setTab] = useState<number>(1)
+  const [tab, setTab] = useState<number>(1);
 
-  const tabs = useRef<HTMLDivElement>(null)
+  const tabs = useRef<HTMLDivElement>(null);
 
   const heightFix = () => {
-    if (tabs.current && tabs.current.parentElement) tabs.current.parentElement.style.height = `${tabs.current.clientHeight}px`
-  }
+    if (tabs.current && tabs.current.parentElement)
+      tabs.current.parentElement.style.height = `${tabs.current.clientHeight}px`;
+  };
 
   useEffect(() => {
-    heightFix()
-  }, []) 
+    heightFix();
+  }, []);
 
   return (
     <section className="relative">
-
       {/* Section background (needs .relative class on parent and next sibling elements) */}
-      <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
-      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
+      <div
+        className="absolute inset-0 bg-gray-100 pointer-events-none mb-16"
+        aria-hidden="true"
+      ></div>
 
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-12 md:pt-20">
-
+        <div className=" md:pt-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">Explore nossas soluções</h1>
-            <p className="text-xl text-gray-600">A Edutech Solutions oferece ótimas soluções para o auxilio de ensino online, transformando a aprendizagem em uma experiência eficiente e envolvente para todos. Seus produtos incluem plataformas de ensino à distância, conteúdo educacional digital e ferramentas de avaliação.</p>
+            <p className="text-xl text-gray-600">
+              A Edutech Solutions oferece ótimas soluções para o auxilio de
+              ensino online, transformando a aprendizagem em uma experiência
+              eficiente e envolvente para todos. Seus produtos incluem
+              plataformas de ensino à distância, conteúdo educacional digital e
+              ferramentas de avaliação.
+            </p>
           </div>
 
           {/* Section content */}
           <div className="md:grid md:grid-cols-12 md:gap-6">
-
             {/* Content */}
-            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6" data-aos="fade-right">
+            <div
+              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6"
+              data-aos="fade-right"
+            >
               <div className="md:pr-4 lg:pr-12 xl:pr-16 mb-8">
                 <h3 className="h3 mb-3">Um ótimo conjunto de ferramentas</h3>
-                <p className="text-xl text-gray-600">Conheça melhor as funcionalidades que oferecemos no nosso principal sistema.</p>
+                <p className="text-xl text-gray-600">
+                  Conheça melhor as funcionalidades que oferecemos no nosso
+                  principal sistema.
+                </p>
               </div>
               {/* Tabs buttons */}
               <div className="mb-8 md:mb-0">
                 <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 1
+                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
+                      : "bg-gray-200 border-transparent"
+                  }`}
                   href="#0"
-                  onClick={(e) => { e.preventDefault(); setTab(1); }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(1);
+                  }}
                 >
                   <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">Cursos disponiveis na plataforma</div>
-                    <div className="text-gray-600">No sistema do UniAcademy pode ser feito o cadastro de cursos para que os alunos de sua instituição possa assistir.</div>
+                    <div className="font-bold leading-snug tracking-tight mb-1">
+                      Cursos disponíveis na plataforma
+                    </div>
+                    <div className="text-gray-600">
+                      No sistema do UniAcademy pode ser feito o cadastro de
+                      cursos para que os alunos de sua instituição possa
+                      assistir.
+                    </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                      className="w-3 h-3 fill-current"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
                       <path d="M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z" />
                     </svg>
                   </div>
                 </a>
                 <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 2 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 2
+                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
+                      : "bg-gray-200 border-transparent"
+                  }`}
                   href="#0"
-                  onClick={(e) => { e.preventDefault(); setTab(2); }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(2);
+                  }}
                 >
                   <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">Mural de aviso</div>
-                    <div className="text-gray-600">Também oferecemos um sistema de avisos para facilitar a comunicação de alunos e professores.</div>
+                    <div className="font-bold leading-snug tracking-tight mb-1">
+                      Mural de aviso
+                    </div>
+                    <div className="text-gray-600">
+                      Também oferecemos um sistema de avisos para facilitar a
+                      comunicação de alunos e professores.
+                    </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" fillRule="nonzero" />
+                    <svg
+                      className="w-3 h-3 fill-current"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z"
+                        fillRule="nonzero"
+                      />
                     </svg>
                   </div>
                 </a>
                 <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 3 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
+                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
+                    tab !== 3
+                      ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
+                      : "bg-gray-200 border-transparent"
+                  }`}
                   href="#0"
-                  onClick={(e) => { e.preventDefault(); setTab(3); }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTab(3);
+                  }}
                 >
                   <div>
-                    <div className="font-bold leading-snug tracking-tight mb-1">Aulas em videos</div>
-                    <div className="text-gray-600">As aulas podem ser assistidas de forma simples e dinamica.</div>
+                    <div className="font-bold leading-snug tracking-tight mb-1">
+                      Aulas em vídeos
+                    </div>
+                    <div className="text-gray-600">
+                      As aulas podem ser assistidas de forma simples e dinâmica.
+                    </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z" fill="#191919" fillRule="nonzero" />
+                    <svg
+                      className="w-3 h-3 fill-current"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z"
+                        fill="#191919"
+                        fillRule="nonzero"
+                      />
                     </svg>
                   </div>
                 </a>
@@ -101,7 +167,11 @@ export default function Features() {
             {/* Tabs items */}
             <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1">
               <div className="transition-all">
-                <div className="relative flex flex-col text-center lg:text-right" data-aos="zoom-y-out" ref={tabs}>
+                <div
+                  className="relative flex flex-col text-center lg:text-right"
+                  data-aos="zoom-y-out"
+                  ref={tabs}
+                >
                   {/* Item 1 */}
                   <Transition
                     show={tab === 1}
@@ -114,10 +184,16 @@ export default function Features() {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 -translate-y-16"
                     beforeEnter={() => heightFix()}
-                    unmount={false}                     
+                    unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                      <Image className="md:max-w-none mx-auto rounded" src={Cursos} width={500} height="462" alt="Features bg" />
+                      <Image
+                        className="md:max-w-none mx-auto rounded"
+                        src={Cursos}
+                        width={500}
+                        height="462"
+                        alt="Features bg"
+                      />
                       {/* <Image className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width={500} height="44" alt="Element" style={{ top: '30%' }} /> */}
                     </div>
                   </Transition>
@@ -133,10 +209,16 @@ export default function Features() {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 -translate-y-16"
                     beforeEnter={() => heightFix()}
-                    unmount={false}                     
+                    unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                      <Image className="md:max-w-none mx-auto rounded" src={Avisos} width={500} height="462" alt="Features bg" />
+                      <Image
+                        className="md:max-w-none mx-auto rounded"
+                        src={Avisos}
+                        width={500}
+                        height="462"
+                        alt="Features bg"
+                      />
                       {/* <Image className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width={500} height="44" alt="Element" style={{ top: '30%' }} /> */}
                     </div>
                   </Transition>
@@ -152,21 +234,25 @@ export default function Features() {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 -translate-y-16"
                     beforeEnter={() => heightFix()}
-                    unmount={false}                     
+                    unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                      <Image className="md:max-w-none mx-auto rounded" src={Aula} width={500} height="462" alt="Features bg" />
+                      <Image
+                        className="md:max-w-none mx-auto rounded"
+                        src={Aula}
+                        width={500}
+                        height="462"
+                        alt="Features bg"
+                      />
                       {/* <Image className="md:max-w-none absolute w-full left-0 transform animate-float" src={FeaturesElement} width={500} height="44" alt="Element" style={{ top: '30%' }} /> */}
                     </div>
                   </Transition>
                 </div>
               </div>
             </div>
-
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,9 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+import VideoThumb from "@/public/images/hero-image.png";
+import ModalVideo from "@/components/modal-video";
 
 export default function Hero() {
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
       {/* <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
@@ -23,39 +22,33 @@ export default function Hero() {
       </div> */}
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Conheça mais sobre a <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#003488] to-[#0061FF]">EduTech solutions</span></h1>
+          <div className="text-center  md:pb-16">
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              Conheça mais sobre a{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#003488] to-[#0061FF]">
+                EduTech solutions
+              </span>
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">A Edutech Solutions é uma inovadora empresa que impulsiona a educação com soluções tecnológicas de ponta, capacitando estudantes e instituições para um futuro mais promissor.</p>
-              {/* <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
-              </div> */}
+              <p
+                className="text-xl text-gray-600 "
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                A Edutech Solutions é uma inovadora empresa que impulsiona a
+                educação com soluções tecnológicas de ponta, capacitando
+                estudantes e instituições para um futuro mais promissor.
+              </p>
             </div>
           </div>
-
-          {/* Hero image */}
-          <ModalVideo
-            thumb={'https://images.unsplash.com/photo-1584907797015-7554cd315667?auto=format&fit=crop&q=80&w=2076&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
-
         </div>
-
       </div>
     </section>
-  )
+  );
 }
